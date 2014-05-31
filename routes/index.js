a9c93d17f3991591acd0bb892e6f50ea5b87051d
Tarcio <tarcio@gmail.com>
2014-05-31 14:02:09 +1000

    Adding routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  var endpoints = {
+    GET: {
+      '/tides': {
+        description: 'Get all king wave tide locations'
+      },
+      '/submissions': {
+        params: ['email'],
+        description: 'Get all king wave tide locations'
+      }
+    },
+    POST: {
+      '/upload': {
+        description: 'Upload a photos'
+      }
+    }
+  }
+  res.json(endpoints);
+});
+
+router.get('/tides', function (req, res) {
+
+});
+
+router.get('/submissions', function (req, res) {
+
+});
+
+router.post('/submit', function (req, res) {
+
 });
 
 module.exports = router;
