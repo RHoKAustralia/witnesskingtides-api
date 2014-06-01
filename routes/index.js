76148d3ff8681f92a475bcfa67c296eb34714683
Tarcio <tarcio@gmail.com>
2014-06-01 12:14:04 +1000

    Fixing upload and tides routes

--- routes/index.js
+++ routes/index.js
@@ -28,38 +28,108 @@ router.get('/', function (req, res) {
 
 router.get('/tides', function (req, res) {
   KingTideEvent.find({}, function (err, events) {
-    var eventMap = {};
-    events.forEach(function(event) {
-        eventMap[event._id] = event;
+    var eventMap = [];
+    events.forEach(function (event) {
+      eventMap.push({
+        "id": event._id,
+        "event": event
+      });
     });
-    res.send(200, eventMap);  
+    res.send(200, eventMap);
   });
 });
 
-router.get('/submissions', function (req, res) {	
-	Photo.find({'user':{'$ne': null }})
-	.populate('user',null,{email:{$in:[req.query.email]}})
-	//.populate('user')
-	//.where('user.email').in(['tarcio@mail.com'])  //not working
-	.exec(function (err, docs) {		
-		if(!docs){
-			res.json({'msg':'no photos with '+req.query.email});
-			return;
-		}
-		var photos = [];
-		//TODO: remove this manual check and do it in the query
-		docs.forEach(function(photo){			
-			if(photo.user && photo.user.email == req.query.email){				
-				photos.push(photo)
-			}
-		});				
-		res.json(photos);
-		// docs is an array
-	});
+router.get('/tides/future/:date?', function (req, res) {
+  var when = req.params['date'] || new Date;
+  kingTideEvent.find({
+    eventStart: {
+      $gte: when
+    }
+  }, function (err, events) {
+    var eventMap = [];
+    events.forEach(function (event) {
+      eventMap.push({
+        "id": event._id,
+        "event": event
+      });
+    });
+    res.send(200, eventMap);
+  });
+});
+
+router.get('/tides/current', function (req, res) {
+  var now = new Date;
+  kingTideEvent.find({
+    eventStart: {
+      $lte: now
+    },
+    eventEnd: {
+      $gte: now
+    }
+  }, function (err, events) {
+    var eventMap = [];
+    events.forEach(function (event) {
+      eventMap.push({
+        "id": event._id,
+        "event": event
+      });
+    });
+    res.send(200, eventMap);
+  });
+});
+
+router.get('/tides/:id', function (req, res) {
+  kingTideEvent.findOne({
+    '_id': req.params['id']
+  }, function (err, event) {
+    if (event == null)
+      res.send(404);
+    else
+      res.send(200, event);
+  });
+});
+
+
+router.get('/submissions', function (req, res) {
+  Photo.find({
+    'user': {
+      '$ne': null
+    }
+  })
+    .populate('user', null, {
+      email: {
+        $in: [req.query.email]
+      }
+    })
+  //.populate('user')
+  //.where('user.email').in(['tarcio@mail.com'])  //not working
+  .exec(function (err, docs) {
+    if (!docs) {
+      res.json({
+        'msg': 'no photos with ' + req.query.email
+      });
+      return;
+    }
+    var photos = [];
+    //TODO: remove this manual check and do it in the query
+    docs.forEach(function (photo) {
+      if (photo.user && photo.user.email == req.query.email) {
+        photos.push(photo)
+      }
+    });
+    res.json(photos);
+    // docs is an array
+  });
 });
 
 router.post('/upload', function (req, res) {
-  new Uploader().go(req, res);
+  var uploader = new Uploader();
+  if (req.get('Content-Type').indexOf('json') >= 0) {
+    uploader.handleJson(req, res);
+  } else {
+    uploader.handleMultipart(req, res);
+  }
+
 });
 
 module.exports = router;
